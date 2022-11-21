82e3ecf78fd91df801fb522cac96e4b162aa7254
Shafiqur Rahman <pallab.alif@gmail.com>
2022-11-22 02:26:06 +0600

    ADD: Exception.

---
+++ array.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val numbers = intArrayOf(1, 2, 3)
+    // DoubleArray
+    val doubles = doubleArrayOf(3.0, 4.0, 5.0)
+
+    println(numbers.contentToString())
+    println(doubles.contentToString())
+}
